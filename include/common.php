4f47ea170954be875c14d5d87c73b7cc63432271
huiyunlee <91522087+huiyunlee@users.noreply.github.com>
2021-09-30 14:33:29 +0800

    Add files via upload

---
+++ include/common.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'ecommerce');
+
+
+?>
