20296f0799694f8a06af7f5ad746bf3041232c6f
Hong Yong <hongyongwhy@users.noreply.github.com>
2021-09-30 15:56:11 +0800

    Fixed getorderitems.php
    -Added customer_id to the query, to get the latest unfulfilled order (status == 0) by that particular customer

--- backend/getorderitems.php
+++ backend/getorderitems.php
@@ -3,9 +3,12 @@ header('Content-Type: application/json');
 
 require 'config.php';
 
-$query = "SELECT product_id, order_id, product_qty, total_price FROM ecommerce.order_item";
+$customer_id = $_GET['customer_id'];
+
+$query = "SELECT o.product_id, p.title, o.order_id, o.product_qty, o.total_price FROM ecommerce.order_item o INNER JOIN ecommerce.product p ON p.id = o.product_id INNER JOIN ecommerce.order od ON od.id = o.order_id WHERE od.customer_id = ? AND od.status = 0";
 
 $stmt = $mysql->prepare($query);
+$stmt->bind_param("i", $customer_id);
 
 $output = array();
 
@@ -13,19 +16,21 @@ if ($stmt->execute()) {
 	$result = $stmt->get_result();
 	while ($row = $result->fetch_assoc()) {
 		$product_id = $row['product_id'];
+		$title = $row['title'];
 		$order_id = $row['order_id'];
 		$product_qty = $row['product_qty'];
 		$total_price = $row['total_price'];
 
     	array_push($output, array(
             'product_id' => $product_id, 
+            'title' => $title, 
             'order_id'=>$order_id, 
             'product_qty'=>$product_qty,
             'total_price'=>$total_price
         ));
 	}
 } else {
-	error_log("Error: " . $sql . "<br>" . $mysql->error);
+	error_log("Error: ". $mysql->error);
 }
 
 $mysql->close();
